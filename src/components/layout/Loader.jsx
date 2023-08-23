8680d602cb0a6e02b1e1f5c46ba2fd6f537036e2
raaz <raaz4995azh@gmail.com>
2023-08-23 11:19:34 +0530

    wait added

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -5,6 +5,8 @@ const Loader = ({color = "teal"}) => {
   return (
     <div>
         <VStack h={'100vh'} justifyContent={'center'} >
+
+            <h1>Please wait Backend server take time for first time </h1>
             <div>
                 <Spinner thickness='8px' speed={'0.45s'} emptyColor='transparent' color={color} size={'xl'}  />
             </div>
